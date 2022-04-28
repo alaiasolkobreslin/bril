0343953de2e8845a0ec89bfbe194b0579571c21e
Alaia Solko-Breslin <ajs644@cornell.edu>
2022-04-28 19:15:38 -0400

    some synthesis not working

--- rosette/l13.rkt
+++ rosette/l13.rkt
@@ -8,7 +8,6 @@
 (struct minus (left right) #:transparent)
 (struct mul (left right) #:transparent)
 (struct div (left right) #:transparent)
-(struct shift (left right) #:transparent)
 (struct square (arg) #:transparent)
 (struct neg (arg) #:transparent)
 (struct absolute (arg) #:transparent)
@@ -18,45 +17,21 @@
 (struct lt (left right) #:transparent)
 (struct gt (left right) #:transparent)
 
-
-(define prog (plus (square 7) 3))
-
 (define (interpret p)
   (destruct p
-    [(iffy a b c) (if (interpret a) (interpret b) (interpret c))]
-    [(plus a b)   (+ (interpret a) (interpret b))]
-    [(minus a b)  (- (interpret a) (interpret b))]
-    [(mul a b)    (* (interpret a) (interpret b))]
-    [(div a b)    (/ (interpret a) (interpret b))]
-    [(shift a b)  (arithmetic-shift (interpret a) (interpret b))]
-    [(square a)   (expt (interpret a) 2)]
-    [(neg a)      (- (interpret a))]
-    [(absolute a) (abs (interpret a))]
+    [(iffy a b c)      (if (interpret a) (interpret b) (interpret c))]
+    [(plus a b)        (+ (interpret a) (interpret b))]
+    [(minus a b)       (- (interpret a) (interpret b))]
+    [(mul a b)         (* (interpret a) (interpret b))]
+    [(div a b)         (/ (interpret a) (interpret b))]
+    [(square a)        (expt (interpret a) 2)]
+    [(neg a)           (- (interpret a))]
+    [(absolute a)      (abs (interpret a))]
 
-    [(eq a b)      (= (interpret a) (interpret b))]
-    [(lt a b)      (< (interpret a) (interpret b))]
-    [(gt a b)      (> (interpret a) (interpret b))]
+    [(eq a b)          (= (interpret a) (interpret b))]
+    [(lt a b)          (< (interpret a) (interpret b))]
+    [(gt a b)          (> (interpret a) (interpret b))]
     [_ p]))
-
-; Compute the absolute value of `x`.
-(define (absv x)
-  (if (< x 0) (- x) x))
-
-(interpret prog)
-
-; Define a symbolic variable called y of type integer.
-(define-symbolic y integer?)
-
-(define-symbolic x c integer?)
-
-(solve 
-  (assert 
-    (= (interpret (square (plus y 2))) 25)))
-
-(synthesize
-  #:forall (list x)
-  #:guarantee (assert (= (interpret (mul c x)) (+ x x))))
-
 
 (define (??expr terminals)
   (define a (apply choose* terminals))
@@ -67,7 +42,6 @@
             (minus a b)
             (mul a b)
             (div a b)
-            (shift a b)
             (square a)
             (absolute a)
 
@@ -75,29 +49,55 @@
             (lt a b)
             (gt a b)))
 
-; (define-symbolic p q integer?)  ; get access to more constants
-; (define sketch
-;   (plus (??expr (list x p q)) (??expr (list x p q))))
+(define-symbolic x integer?)
+(define-symbolic i j integer?)  ; get access to constants
+(define sketch1
+  (iffy (??expr (list x i j)) x (??expr (list x i j))))
 
-; (define M
-;   (synthesize
-;     #:forall (list x)
-;     #:guarantee (assert (= (interpret sketch) (interpret (mul 10 x))))))
-
-; (evaluate sketch M)
-
-; (define f -16)
-; (interpret (shift f 1))
-
-(define-symbolic u integer?)
-(define-symbolic i integer?)  ; get access to more constants
-(define sketch_new
-  (absolute (??expr (list u i))))
-  ; (shift u 1))
-
-(define Z
+(define M
   (synthesize
-    #:forall (list u)
-    #:guarantee (assert (= (interpret sketch_new) (interpret (iffy (gt u 0) u (neg u)))))))
+    #:forall (list x)
+    #:guarantee (assert (= (interpret sketch1) (interpret (absolute x))))))
 
-(evaluate sketch_new Z)
+(evaluate sketch1 M)
+
+
+
+(define-symbolic y integer?)
+(define-symbolic a b integer?)  ; get access to constants
+(define sketch2
+  (mul (??expr (list y a b)) y))
+
+(define N
+  (synthesize
+    #:forall (list y)
+    #:guarantee (assert (= (interpret sketch2) (interpret (square y))))))
+
+(evaluate sketch2 N)
+
+
+(define-symbolic z integer?)
+(define-symbolic c d integer?)  ; get access to constants
+(define sketch3
+  (absolute (??expr (list z c d))))
+
+(define O
+  (synthesize
+    #:forall (list z)
+    #:guarantee (assert (= (interpret sketch3) (interpret (iffy (gt z 0) z (neg z)))))))
+
+(evaluate sketch3 O)
+
+(define-symbolic e integer?)
+(define-symbolic f g integer?)  ; get access to constants
+(define sketch4
+  (mul (??expr (list e f g)) (??expr (list e f g))))
+
+(define P
+  (synthesize
+    #:forall (list e)
+    #:guarantee (assert (= (interpret sketch4) (mul 2 (mul 2 e))))))
+
+(evaluate sketch4 P)
+
+
